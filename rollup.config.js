7870f5eb1eac119a2dd98150a4767cfc4af61d63
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2020-08-04 09:30:41 -0500

    Consider prop types an external dependency

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import pkg from './package.json';
 export default {
     input: 'src/index.js',
     plugins: [nodeResolve(), babel({ presets: ['@babel/preset-react'] })],
-    external: ['react'],
+    external: ['react', 'prop-types'],
     output: [
         {
             file: pkg.module,
