2fa74ed4ff3be25380444a0f37f269ae68d8c0f6
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2020-08-06 13:31:15 -0500

    Update browser support with babel-preset-env
    Also, add new output file for CDN

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,19 @@
-import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import babel from '@rollup/plugin-babel';
 import replace from '@rollup/plugin-replace';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
 
 export default {
     input: 'src/main.js',
     plugins: [
-        getBabelOutputPlugin({
-            presets: ['@babel/preset-env']
+        nodeResolve({
+            browser: true
+        }),
+        commonjs(),
+        babel({
+            babelHelpers: 'bundled',
+            exclude: /node_modules/
         }),
         replace({
             '__VERSION__': pkg.version
@@ -20,6 +27,13 @@ export default {
         {
             file: pkg.main,
             format: 'cjs'
+        },
+        {
+            file: 'dist/paypal.browser.js',
+            format: 'iife',
+            name: 'paypalLoadScript',
+            // update the default export to be the loadScript() function
+            footer: 'paypalLoadScript = paypalLoadScript.loadScript;'
         }
     ]
 };
