2abd2a5b2ba313f169d0aba68224f20a24214273
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2020-11-23 12:57:10 -0600

    Update promise polyfill strategy to be configurable

--- src/loadScript.js
+++ src/loadScript.js
@@ -1,20 +1,29 @@
-import Promise from 'promise-polyfill';
 import { findScript, insertScriptElement, processOptions } from './utils';
 
 const SDK_BASE_URL = 'https://www.paypal.com/sdk/js';
 let loadingPromise;
 let isLoading = false;
 
-export function loadScript(options) {
+export default function loadScript(options, PromisePonyfill) {
     // resolve with the existing promise when the script is loading
     if (isLoading) return loadingPromise;
 
-    return loadingPromise = new Promise((resolve, reject) => {
+    if (typeof PromisePonyfill === 'undefined') {
+        // default to using window.Promise as the Promise implementation
+        if (typeof Promise === 'undefined') {
+            throw new Error('Failed to load the PayPal JS SDK script because Promise is undefined. To resolve the issue, use a Promise polyfill.');
+        }
+
+        PromisePonyfill = Promise;
+    }
+
+    return loadingPromise = new PromisePonyfill((resolve, reject) => {
         // resolve with null when running in Node
         if (typeof window === 'undefined') return resolve(null);
 
         const { queryString, dataAttributes } = processOptions(options);
-        const url = `${SDK_BASE_URL}?${queryString}`;
+        const sdkBaseURL = options.sdkBaseURL || SDK_BASE_URL;
+        const url = `${sdkBaseURL}?${queryString}`;
 
         // resolve with the existing global paypal object when a script with the same src already exists
         if (findScript(url, dataAttributes) && window.paypal) return resolve(window.paypal);
@@ -36,6 +45,3 @@ export function loadScript(options) {
         });
     });
 }
-
-// replaced with the package.json version at build time
-export const version = '__VERSION__';
