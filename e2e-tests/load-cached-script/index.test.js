99a7c8a8c2a7b6a4cb46be6539b1bf395b208ebf
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2021-03-18 12:35:07 -0500

    fix: ignore data-uid-auto attribute in findScript

---
+++ e2e-tests/load-cached-script/index.test.js
@@ -0,0 +1,55 @@
+import { baseURL } from "../test-helper";
+
+describe("Load cached script", () => {
+    beforeEach(async () => {
+        await Promise.all([
+            page.goto(`${baseURL}/e2e-tests/load-cached-script/index.html`, {
+                waitUntil: "networkidle2",
+            }),
+            page.waitForResponse((response) =>
+                response.url().startsWith("https://www.paypal.com/sdk/js")
+            ),
+            page.waitForResponse((response) =>
+                response
+                    .url()
+                    .startsWith("https://www.sandbox.paypal.com/smart/buttons")
+            ),
+        ]);
+    });
+
+    it("should return the expected page <title>", async () => {
+        const pageTitle = await page.title();
+        expect(pageTitle).toBe("Load Cached Script | PayPal JS");
+    });
+
+    it("should not reload the script when the loadScript options have not changed", async () => {
+        const scriptElement = await page.$(
+            'script[src^="https://www.paypal.com/sdk/js"]'
+        );
+        const scriptUID = await page.evaluate(
+            (el) => el.getAttribute("data-uid-auto"),
+            scriptElement
+        );
+
+        const reloadButton = await page.evaluateHandle(() =>
+            document.querySelector("#btn-reload")
+        );
+        await reloadButton.click();
+
+        await page.waitForResponse((response) =>
+            response
+                .url()
+                .startsWith("https://www.sandbox.paypal.com/smart/buttons")
+        );
+
+        const latestScriptElement = await page.$(
+            'script[src^="https://www.paypal.com/sdk/js"]'
+        );
+        const latestScriptUID = await page.evaluate(
+            (el) => el.getAttribute("data-uid-auto"),
+            latestScriptElement
+        );
+
+        expect(latestScriptUID).toBe(scriptUID);
+    });
+});
