191c5c63b9a23e3ff69677e05756ecc1919828ed
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2021-07-09 09:21:50 -0500

    chore(release): use precommit hook to bump version in readme

---
+++ scripts/readme-package-version.js
@@ -0,0 +1,13 @@
+const { readFileSync, writeFileSync } = require("fs");
+const { join } = require("path");
+
+const { version: newVersion } = require("../package.json");
+
+const readmeFile = join(__dirname, "../", "README.md");
+const readmeText = readFileSync(readmeFile, "utf-8");
+const newReadmeText = readmeText.replace(
+    /[0-9]+\.[0-9]+\.[0-9]+\/dist\//g,
+    `${newVersion}/dist/`
+);
+
+writeFileSync(readmeFile, newReadmeText, "utf-8");
