65c5302276925a920ae2bec19aa7aebef8d0aaae
Liam Snow <liamsnow03@gmail.com>
2021-11-24 09:24:31 -0800

    fix: add types for onShippingChange callback (#135)

---
+++ types/apis/shipping.d.ts
@@ -0,0 +1,17 @@
+export type ShippingOptionType = 'SHIPPING' | 'PICKUP';
+
+export type ShippingAddress = {
+    city: string,
+    state: string,
+    country_code: string,
+    postal_code: string
+};
+
+export type SelectedShippingOption = {
+    label: string,
+    type: ShippingOptionType,
+    amount: {
+        currency_code: string,
+        value: string
+    }
+};
