c55988a265fb75e18a947bd4f6c7b19f022a778d
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2022-01-20 16:33:23 -0600

    chore: use semver for node version check (#155)

--- scripts/check-node-version.js
+++ scripts/check-node-version.js
@@ -1,69 +1,26 @@
 const { readFileSync } = require("fs");
 const { join } = require("path");
 const chalk = require("chalk");
-const check = require("check-node-version");
+const semver = require("semver");
 
-const nodeVersion = readFileSync(join(__dirname, "../", ".nvmrc"), "utf-8");
+const expectedNodeVersion = readFileSync(
+    join(__dirname, "../", ".nvmrc"),
+    "utf-8"
+);
+const isValidNodeVersion = semver.satisfies(
+    process.version,
+    expectedNodeVersion
+);
 
-check({ node: nodeVersion }, (err, result) => {
-    if (err) {
-        throw err;
-    }
-
-    printVersions(result);
-    process.exit(result.isSatisfied ? 0 : 1);
-});
-
-// printVersions and printInstalledVersion were copied from:
-// https://github.com/parshap/check-node-version/blob/master/cli.js
-
-function printVersions(result) {
-    Object.keys(result.versions).forEach((name) => {
-        const info = result.versions[name];
-        const isSatisfied = info.isSatisfied;
-
-        // print installed version
-        if (!isSatisfied) {
-            printInstalledVersion(name, info);
-        }
-
-        if (isSatisfied) return;
-
-        // report any non-compliant versions
-        const { raw, range } = info.wanted;
-
-        console.error(
-            chalk.red(
-                `Wanted ${name} version ` + chalk.bold(`${raw} (${range})`)
-            ),
-            "\n"
-        );
-    });
+// successfully exit when Node version is valid
+if (isValidNodeVersion) {
+    return;
 }
 
-function printInstalledVersion(
-    name,
-    { version, isSatisfied, invalid, notfound }
-) {
-    let versionNote = "";
+const output = `
+node: ${chalk.bold(process.version)}
+Wanted node version ${chalk.bold(expectedNodeVersion)}
+`;
 
-    if (version) {
-        versionNote = name + ": " + chalk.bold(version);
-    }
-
-    if (invalid) {
-        versionNote =
-            name + ": " + chalk.bold("given version not semver-compliant");
-    }
-
-    if (notfound) {
-        versionNote = name + ": not found";
-    }
-
-    if (isSatisfied) {
-        if (version) console.log(versionNote);
-        else console.log(chalk.gray(versionNote));
-    } else {
-        console.log(chalk.red(versionNote));
-    }
-}
+console.error(chalk.red(output));
+process.exit(1);
