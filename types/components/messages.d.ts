1d2c453a94ec391932a3af3056b1c1615e2f435a
Asriel Yu <luffy84217@gmail.com>
2022-01-29 09:36:25 +0800

    chore(lint): update `messages.d.ts`

--- types/components/messages.d.ts
+++ types/components/messages.d.ts
@@ -5,14 +5,14 @@ export interface PayPalMessagesComponentOptions {
         layout?: "text" | "flex" | "custom";
         color?: string;
         logo?: {
-            type?: 'primary' | 'alternative' | 'inline' | 'none';
-            position?: 'left' | 'right' | 'top';
+            type?: "primary" | "alternative" | "inline" | "none";
+            position?: "left" | "right" | "top";
         };
         text?: {
-            color?: 'black' | 'white' | 'monochrome' | 'grayscale';
-            size?: 	10 | 11 | 12 | 13 | 14 | 15 | 16;
-            align?: 'left' | 'center' | 'right'
-        }
+            color?: "black" | "white" | "monochrome" | "grayscale";
+            size?: 10 | 11 | 12 | 13 | 14 | 15 | 16;
+            align?: "left" | "center" | "right";
+        };
     };
 }
 
