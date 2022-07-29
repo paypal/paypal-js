dd6bc48d0755efcd5ad529e92322cd1ec051d84c
CoderBrad <58267466+CoderBrad@users.noreply.github.com>
2022-07-29 11:22:50 -0500

    fix(types): add missing types for messaging component (#274)

--- types/components/messages.d.ts
+++ types/components/messages.d.ts
@@ -1,6 +1,6 @@
 export interface PayPalMessagesComponentOptions {
-    account?: string;
     amount?: number | string;
+    currency?: "USD" | "GBP" | "EUR";
     style?: {
         layout?: "text" | "flex" | "custom";
         color?: string;
@@ -13,7 +13,12 @@ export interface PayPalMessagesComponentOptions {
             size?: 10 | 11 | 12 | 13 | 14 | 15 | 16;
             align?: "left" | "center" | "right";
         };
+        ratio?: "1x1" | "1x4" | "8x1" | "20x1";
     };
+    placement?: "home" | "category" | "product" | "cart" | "payment";
+    onApply?: (data: Record<string, unknown>) => void;
+    onClick?: (data: Record<string, unknown>) => void;
+    onRender?: (data: Record<string, unknown>) => void;
 }
 
 export interface PayPalMessagesComponent {
