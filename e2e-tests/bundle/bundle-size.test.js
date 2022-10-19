2682b2209c237a36aa847096a088c3b2a44fc65b
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2022-10-19 15:54:27 -0500

    chore(tests): use a named export with filesize dep

--- e2e-tests/bundle/bundle-size.test.js
+++ e2e-tests/bundle/bundle-size.test.js
@@ -1,7 +1,7 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
 
 const fs = require("fs");
-const filesize = require("filesize");
+const { filesize } = require("filesize");
 
 const maxBundleSizeInKiloBytes = 4;
 const maxLegacyBundleSizeInKiloBytes = 7.5;
