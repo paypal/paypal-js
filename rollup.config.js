e6cd97ecf52ffc17477b5179fdbab91c4d533de3
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2022-11-11 10:49:31 -0600

    chore(deps): upgrade to rollup 3 (#300)

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import replace from "@rollup/plugin-replace";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import pkg from "./package.json";
 
