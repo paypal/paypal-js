0d9b495a15e2fd8e923bc55237bb6ba0c69cd11e
Nate Bierdeman <nbierdeman@paypal.com>
2024-03-20 14:12:40 -0400

    fix: alphabetize constants

--- src/constants.ts
+++ src/constants.ts
@@ -7,10 +7,10 @@ export const SCRIPT_ID = "data-react-paypal-script-id";
 
 export const SDK_SETTINGS = {
     DATA_CLIENT_TOKEN: "dataClientToken",
-    DATA_USER_ID_TOKEN: "dataUserIdToken",
-    DATA_SDK_INTEGRATION_SOURCE: "dataSdkIntegrationSource",
     DATA_LIBRARY_VALUE: "react-paypal-js",
     DATA_NAMESPACE: "dataNamespace",
+    DATA_SDK_INTEGRATION_SOURCE: "dataSdkIntegrationSource",
+    DATA_USER_ID_TOKEN: "dataUserIdToken",
 };
 export const LOAD_SCRIPT_ERROR = "Failed to load the PayPal JS SDK script.";
 
