eb187a8e5f83ddcf6ee9f30f2f770d40f6ec99cd
Sebastian Fernandez <sebfernandez@paypal.com>
2024-04-18 11:57:08 -0600

    chore: remove style block and replace with cusotm container

--- src/components/cardFields/PayPalCardFieldsForm.tsx
+++ src/components/cardFields/PayPalCardFieldsForm.tsx
@@ -3,34 +3,24 @@ import React from "react";
 import { PayPalCardFieldsFormOptions } from "../../types";
 import { PayPalCardField } from "./PayPalCardField";
 import { FlexContainer } from "../ui/FlexContainer";
+import { FullWidthContainer } from "../ui/FullWidthContainer";
 
 export const PayPalCardFieldsForm: React.FC<PayPalCardFieldsFormOptions> = ({
     className,
     ...options
 }) => {
     return (
-        <>
-            <style
-                dangerouslySetInnerHTML={{
-                    __html: `.w-full { width:100%; }`,
-                }}
-            />
-            <div className={className}>
-                <PayPalCardField fieldName="NameField" {...options} />
-                <PayPalCardField fieldName="NumberField" {...options} />
-                <FlexContainer>
-                    <PayPalCardField
-                        fieldName="ExpiryField"
-                        className="w-full"
-                        {...options}
-                    />
-                    <PayPalCardField
-                        fieldName="CVVField"
-                        className="w-full"
-                        {...options}
-                    />
-                </FlexContainer>
-            </div>
-        </>
+        <div className={className}>
+            <PayPalCardField fieldName="NameField" {...options} />
+            <PayPalCardField fieldName="NumberField" {...options} />
+            <FlexContainer>
+                <FullWidthContainer>
+                    <PayPalCardField fieldName="ExpiryField" {...options} />
+                </FullWidthContainer>
+                <FullWidthContainer>
+                    <PayPalCardField fieldName="CVVField" {...options} />
+                </FullWidthContainer>
+            </FlexContainer>
+        </div>
     );
 };
