35591cd0def4b24137ac71f6987ea3306175223b
Hai-Yen Nguyen <haiynguyen@paypal.com>
2025-01-22 13:11:17 -0600

    fix naming for the test and update  changeset

---
+++ .changeset/modern-drinks-think.md
@@ -0,0 +1,5 @@
+---
+"@paypal/paypal-js": minor
+---
+
+Adding test for card-fields.test.ts
--- packages/paypal-js/types/tests/card-fields.test.ts
+++ packages/paypal-js/types/tests/card-fields.test.ts
@@ -2,7 +2,7 @@ import { test, vi, describe, expect } from "vitest";
 
 import { PayPalCardFieldsComponentOptions } from "../components/card-fields";
 
-describe("testing", () => {
+describe.only("testing createOrder and createVaultToken types", () => {
     test("only creatOrder", () => {
         const createOrderCallback: PayPalCardFieldsComponentOptions = {
             createOrder: vi.fn(),
@@ -24,8 +24,8 @@ describe("testing", () => {
         expect(createVaultSetupTokenCallback.createOrder).toBeUndefined();
     });
 
-    test.skip("Can't have both", () => {
-        // @ts-expect-error : should throw error if both createOrder and createVaultSetuptoken called
+    test.skip("Can't have both createOrdre and createVaultSetupToken", () => {
+        // @ts-expect-error : should throw error if both createOrder and createVaultSetupToken called
         const callback: PayPalCardFieldsComponentOptions = {
             createOrder: vi.fn(),
             createVaultSetupToken: vi.fn(),
@@ -36,7 +36,7 @@ describe("testing", () => {
         expect(callback).toThrowError();
     });
 
-    test.skip("Can't have both", () => {
+    test.skip("Can't implement neither", () => {
         // @ts-expect-error: should throw error if neither createOrder or createVaultSetuptoken called
         const callback: PayPalCardFieldsComponentOptions = {
             onApprove: vi.fn(),
