d3851e43e001c272289b8a50f8ec6d5b4e3410dc
Brian Tedder <6571474+imbrian@users.noreply.github.com>
2025-09-02 16:53:29 -0500

    (chore) add types for PPCP v5 App Switch (#669)
    Adds types for direct app switch support in v5.
    
    ---------
    
    Co-authored-by: Ravi Shekhar <ravishekhar@users.noreply.github.com>

---
+++ .changeset/mean-chefs-love.md
@@ -0,0 +1,5 @@
+---
+"@paypal/paypal-js": patch
+---
+
+Adds types for v5 Direct App Switch
--- packages/paypal-js/types/components/buttons.d.ts
+++ packages/paypal-js/types/components/buttons.d.ts
@@ -350,6 +350,10 @@ export interface PayPalButtonsComponentOptions {
      * [Message options](https://developer.paypal.com/sdk/js/reference/#message) for customizing the message appearance and limited content control.
      */
     message?: PayPalButtonMessage;
+    /**
+     * When true, clicking the button will first attempt to open the paysheet in the PayPal app (if supported). If not supported, Checkout will open in the web flow.
+     */
+    appSwitchWhenAvailable?: boolean;
 }
 
 export interface PayPalButtonsComponent {
@@ -357,4 +361,6 @@ export interface PayPalButtonsComponent {
     isEligible: () => boolean;
     render: (container: HTMLElement | string) => Promise<void>;
     updateProps: (props: PayPalButtonsComponentOptions) => Promise<void>;
+    resume: () => void;
+    hasReturned: () => boolean;
 }
