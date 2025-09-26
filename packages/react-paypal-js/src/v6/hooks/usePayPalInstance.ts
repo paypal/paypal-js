414227f5410b597d14657602c95c650e26fe8520
Evan Reinstein <42251756+EvanReinstein@users.noreply.github.com>
2025-09-26 09:09:07 -0500

    clean up usePayPalInstance file

--- packages/react-paypal-js/src/v6/hooks/usePayPalInstance.ts
+++ packages/react-paypal-js/src/v6/hooks/usePayPalInstance.ts
@@ -3,11 +3,6 @@ import { useContext } from "react";
 import { InstanceContext } from "../context/InstanceProviderContext";
 
 import type { InstanceContextState } from "../types/InstanceProviderTypes";
-import type {
-    Components,
-    EligiblePaymentMethodsOutput,
-    SdkInstance,
-} from "../types";
 
 export function usePayPalInstance(): InstanceContextState {
     const context = useContext(InstanceContext);
@@ -19,26 +14,4 @@ export function usePayPalInstance(): InstanceContextState {
     }
 
     return context;
-}
-
-export function usePayPalSdkInstance(): SdkInstance<
-    readonly [Components, ...Components[]]
-> | null {
-    const { sdkInstance } = usePayPalInstance();
-    return sdkInstance;
-}
-
-export function usePayPalEligibleMethods(): EligiblePaymentMethodsOutput | null {
-    const { eligiblePaymentMethods } = usePayPalInstance();
-    return eligiblePaymentMethods;
-}
-
-export function usePayPalLoading(): boolean {
-    const { isLoading } = usePayPalInstance();
-    return isLoading;
-}
-
-export function usePayPalError(): Error | null {
-    const { error } = usePayPalInstance();
-    return error;
 }
