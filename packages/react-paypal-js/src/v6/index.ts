735e680f046abd7c25a80ebe00bddbffc18ce758
Evan Reinstein <42251756+EvanReinstein@users.noreply.github.com>
2025-10-10 10:06:45 -0500

    add inital paypal otp payment session hook

--- packages/react-paypal-js/src/v6/index.ts
+++ packages/react-paypal-js/src/v6/index.ts
@@ -2,3 +2,4 @@ export * from "./types";
 export { PayPalProvider } from "./components/PayPalProvider";
 export { PayPalContext } from "./context/PayPalProviderContext";
 export { usePayPal } from "./hooks/usePayPal";
+export { usePayPalOneTimePaymentSession } from "./hooks/usePayPalOneTimePaymentSession";
