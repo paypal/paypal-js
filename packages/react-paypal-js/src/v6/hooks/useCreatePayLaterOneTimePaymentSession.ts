caeb96527a9f35c3781bb08b2a366f027010b1de
Andrew Kos <akos123@gmail.com>
2025-10-17 11:09:37 -0500

    wip

--- packages/react-paypal-js/src/v6/hooks/useCreatePayLaterOneTimePaymentSession.ts
+++ packages/react-paypal-js/src/v6/hooks/useCreatePayLaterOneTimePaymentSession.ts
@@ -1,45 +1,76 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 import { usePayPal } from "./usePayPal";
-import { useDeepCompareMemoize } from "../utils";
 
 import type {
     OneTimePaymentSession,
-    PayLaterOneTimePaymentSessionOptions,
+    PayPalPresentationModeOptions,
+    PayLaterOneTimePaymentSessionProps,
+    PayLaterOneTimePaymentSessionReturn,
 } from "../types";
 
 // TODO tests
 // TODO example
 // TODO eligibility
-export function usePayLaterOneTimePaymentSession(
-    options: PayLaterOneTimePaymentSessionOptions,
-): OneTimePaymentSession | null {
+export function usePayLaterOneTimePaymentSession({
+    presentationMode,
+    createOrder,
+    orderId,
+    ...callbacks
+}: PayLaterOneTimePaymentSessionProps): PayLaterOneTimePaymentSessionReturn {
     const { sdkInstance } = usePayPal();
     const sessionRef = useRef<OneTimePaymentSession | null>(null); // handle cleanup
-    const [session, updateSession] = useState<OneTimePaymentSession | null>(
-        null,
-    ); // handle session storage and force re-render
-    const memoizedOptions = useDeepCompareMemoize(options);
+
+    // TODO useProxProps
+    // TODO which props should be exposed for this session type?
+
+    const handleDestroy = useCallback(() => {
+        if (sessionRef.current) {
+            sessionRef.current.destroy();
+            sessionRef.current = null;
+        }
+    }, []);
 
     useEffect(() => {
-        if (sdkInstance) {
-            const newSession =
-                sdkInstance.createPayLaterOneTimePaymentSession(
-                    memoizedOptions,
-                );
-            sessionRef.current = newSession;
-            updateSession(newSession);
-        } else {
+        if (!sdkInstance) {
             // TODO what if sdk instance is not available? Error?
             throw new Error("no sdk instance available");
         }
 
-        return () => {
-            sessionRef.current?.destroy();
-            sessionRef.current = null;
-            updateSession(null);
-        };
-    }, [sdkInstance, memoizedOptions]);
+        const newSession = sdkInstance.createPayLaterOneTimePaymentSession({
+            orderId,
+            ...callbacks,
+        });
+        sessionRef.current = newSession;
 
-    return session;
+        return handleDestroy;
+    }, [sdkInstance, orderId, callbacks, handleDestroy]);
+
+    const handleCancel = useCallback(() => {
+        if (sessionRef.current) {
+            sessionRef.current.cancel();
+        }
+    }, []);
+
+    const handleClick = useCallback(async () => {
+        if (!sessionRef.current) {
+            throw new Error("Venmo session not available");
+        }
+
+        const startOptions: PayPalPresentationModeOptions = {
+            presentationMode,
+        };
+
+        if (createOrder) {
+            await sessionRef.current.start(startOptions, createOrder());
+        } else {
+            await sessionRef.current.start(startOptions);
+        }
+    }, [createOrder, presentationMode]);
+
+    return {
+        handleCancel,
+        handleClick,
+        handleDestroy,
+    };
 }
---
+++ packages/react-paypal-js/src/v6/types/PayLaterOneTimePaymentSession.ts
@@ -0,0 +1,23 @@
+import type { BasePaymentSessionPromise } from "@paypal/paypal-js/sdk-v6";
+import type {
+    PayLaterOneTimePaymentSessionOptions,
+    PayPalPresentationModeOptions,
+} from "./";
+
+export type PayLaterOneTimePaymentSessionProps =
+    | (Omit<PayLaterOneTimePaymentSessionOptions, "orderId"> & {
+          createOrder: () => BasePaymentSessionPromise;
+          presentationMode: PayPalPresentationModeOptions["presentationMode"];
+          orderId?: never;
+      })
+    | (PayLaterOneTimePaymentSessionOptions & {
+          createOrder?: never;
+          presentationMode: PayPalPresentationModeOptions["presentationMode"];
+          orderId: string;
+      });
+
+export interface PayLaterOneTimePaymentSessionReturn {
+    handleClick: () => Promise<void>;
+    handleCancel: () => void;
+    handleDestroy: () => void;
+}
