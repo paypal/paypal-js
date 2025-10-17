e99e7ff8d78f43316c30b9607224956231baf53e
Andrew Kos <akos123@gmail.com>
2025-10-17 15:59:40 -0500

    test stubs

--- packages/react-paypal-js/src/v6/hooks/useCreatePayLaterOneTimePaymentSession.test.ts
+++ packages/react-paypal-js/src/v6/hooks/useCreatePayLaterOneTimePaymentSession.test.ts
@@ -1,7 +1,35 @@
-// TODO tests
-
 describe("usePayLaterOneTimePaymentSession", () => {
-    it("should fail", () => {
+    it("should error if there is no sdkInstance when called", () => {
+        // TODO implement
+        throw new Error("implement");
+    });
+
+    it("should provide a cancel handler that cancels the session", () => {
+        // TODO implement
+        throw new Error("implement");
+    });
+
+    it("should provide a click handler that calls session start", () => {
+        // TODO implement
+        throw new Error("implement");
+    });
+
+    it("should error if the click handler is called and there is no sdkInstance", () => {
+        // TODO implement
+        throw new Error("implement");
+    });
+
+    it("should provide a destroy handler that destroys the session", () => {
+        // TODO implement
+        throw new Error("implement");
+    });
+
+    it("should not re-run if callbacks are updated", () => {
+        // TODO implement
+        throw new Error("implement");
+    });
+
+    it("should destroy the session when the parent component is unmounted", () => {
         // TODO implement
         throw new Error("implement");
     });
