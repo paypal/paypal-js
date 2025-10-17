90e37c4c528d1f5e6283ffaac2f62a2635404bab
Andrew Kos <akos123@gmail.com>
2025-10-17 11:09:37 -0500

    wip

--- packages/react-paypal-js/src/v6/types/index.ts
+++ packages/react-paypal-js/src/v6/types/index.ts
@@ -1,3 +1,4 @@
-export type * from "@paypal/paypal-js/sdk-v6";
-export type * from "./PayPalProviderTypes";
+export * from "./PayLaterOneTimePaymentSession";
 export * from "./PayPalProviderEnums";
+export type * from "./PayPalProviderTypes";
+export type * from "@paypal/paypal-js/sdk-v6";
