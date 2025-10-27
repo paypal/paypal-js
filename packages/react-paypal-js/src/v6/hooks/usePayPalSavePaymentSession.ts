1e04671f3932bed38fdf69f553b385ec73be3549
Evan Reinstein <42251756+EvanReinstein@users.noreply.github.com>
2025-10-27 12:25:27 -0500

    Feat: usePayPalSavePaymentSession hook  (#708)
    * add paypal save payment session hook
    
    * fix typo
    
    * fix another typo

---
+++ packages/react-paypal-js/src/v6/hooks/usePayPalSavePaymentSession.ts
@@ -0,0 +1,67 @@
+import { useCallback, useEffect, useRef } from "react";
+
+import { usePayPal } from "./usePayPal";
+import { useProxyProps } from "../utils";
+
+import type {
+    SavePaymentSession,
+    PayPalPresentationModeOptions,
+    PayPalSavePaymentSessionProps,
+    PayPalSavePaymentSessionReturn,
+} from "../types";
+
+export function usePayPalSavePaymentSession({
+    presentationMode,
+    createVaultToken,
+    vaultSetupToken,
+    ...callbacks
+}: PayPalSavePaymentSessionProps): PayPalSavePaymentSessionReturn {
+    const { sdkInstance } = usePayPal();
+    const sessionRef = useRef<SavePaymentSession | null>(null); // handle cleanup
+    const proxyCallbacks = useProxyProps(callbacks);
+
+    const handleDestroy = useCallback(() => {
+        sessionRef.current?.destroy();
+        sessionRef.current = null;
+    }, []);
+
+    useEffect(() => {
+        if (!sdkInstance) {
+            throw new Error("no sdk instance available");
+        }
+
+        const newSession = sdkInstance.createPayPalSavePaymentSession({
+            vaultSetupToken,
+            ...proxyCallbacks,
+        });
+        sessionRef.current = newSession;
+
+        return handleDestroy;
+    }, [sdkInstance, vaultSetupToken, proxyCallbacks, handleDestroy]);
+
+    const handleCancel = useCallback(() => {
+        sessionRef.current?.cancel();
+    }, []);
+
+    const handleClick = useCallback(async () => {
+        if (!sessionRef.current) {
+            throw new Error("save payment session not available");
+        }
+
+        const startOptions: PayPalPresentationModeOptions = {
+            presentationMode,
+        };
+
+        if (createVaultToken) {
+            await sessionRef.current.start(startOptions, createVaultToken());
+        } else {
+            await sessionRef.current.start(startOptions);
+        }
+    }, [createVaultToken, presentationMode]);
+
+    return {
+        handleCancel,
+        handleClick,
+        handleDestroy,
+    };
+}
