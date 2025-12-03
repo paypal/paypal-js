79e9533edaa4c1f97ec828ed39afe16c81918215
akos_paypal <akos@paypal.com>
2025-12-03 11:31:31 -0600

    rename sdkWebComponents

--- packages/react-paypal-js/src/v6/types/index.ts
+++ packages/react-paypal-js/src/v6/types/index.ts
@@ -1,7 +1,7 @@
 export * from "./PayPalProviderEnums";
 
 export type * from "@paypal/paypal-js/sdk-v6";
-export type * from "./sdkButtons";
+export type * from "./sdkWebComponents";
 
 export interface BasePaymentSessionReturn {
     error: Error | null;
