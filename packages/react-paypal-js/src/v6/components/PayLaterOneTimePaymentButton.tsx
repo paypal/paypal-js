853cfb714fc7ada2f6bdef4428bbac96ef1e2771
Nitya <nsattaru@paypal.com>
2025-12-16 11:20:38 -0600

    simplifying disabled prop logic using double negation

--- packages/react-paypal-js/src/v6/components/PayLaterOneTimePaymentButton.tsx
+++ packages/react-paypal-js/src/v6/components/PayLaterOneTimePaymentButton.tsx
@@ -57,7 +57,7 @@ export const PayLaterOneTimePaymentButton = ({
             onClick={handleClick}
             countryCode={countryCode}
             productCode={productCode}
-            disabled={disabled || error !== null ? true : undefined}
+            disabled={disabled || !!error || undefined}
         ></paypal-pay-later-button>
     );
 };
