c879cb2cf4ae4bebe52cc2c7e07c77fbc4df0e98
Drew Liu <drewliu96@gmail.com>
2026-02-09 11:43:13 -0600

    deploy storybook

--- packages/react-paypal-js-storybook/v6/.storybook/main.ts
+++ packages/react-paypal-js-storybook/v6/.storybook/main.ts
@@ -17,6 +17,12 @@ const config: StorybookConfig = {
             },
         },
     },
+    viteFinal: async (config) => {
+        if (process.env.GITHUB_PAGES === "true") {
+            config.base = "/paypal-js/web-sdk-v6-react-storybook/";
+        }
+        return config;
+    },
 };
 
 export default config;
